e9af7c926ca7368cd1c392b539a913acc197d362
한동희 / 서비스개발담당 WEB 개발팀 <oper0116@ssg.com>
2023-02-23 22:43:32 +0900

    feat: 초기 커밋

---
+++ app.js
@@ -0,0 +1,23 @@
+const { Builder, By, Key, until, Browser } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const options = new chrome.Options({ headless: true });
+const fs = require("fs");
+
+(async function example() {
+  let driver = await new Builder()
+    .forBrowser(Browser.CHROME)
+    .setChromeOptions(options.headless())
+    .build();
+  try {
+    // 네이버 실행
+    await driver.get("https://www.google.com/ncr");
+    await driver.findElement(By.name("q")).sendKeys("webdriver", Key.RETURN);
+
+    await driver.wait(until.titleIs("webdriver - Google Search"), 1000);
+
+    let base64 = await driver.printPage({ pageRanges: ["1-2"] });
+    await fs.writeFileSync("./test.pdf", base64, "base64");
+  } finally {
+    driver.quit();
+  }
+})();
